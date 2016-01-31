bb46c134a39d7661213a834b40bc18a26212b71b
lesspion <coquelet.c@gmail.com>
2016-01-31 23:51:09 +0100

    generic template engine loader

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server();
+var swig = require('swig');
 process.myEnv = require('./MyEnv');
+var templateEngine = require(process.myEnv.config_folder + '/templateEngine');
 var load = require(process.myEnv.tools_folder + '/loader');
 
 server.connection({ 
@@ -8,19 +10,22 @@ server.connection({
     port: process.env.PORT || process.myEnv.port || 8080
 });
 
+// Extending Swig
+
+
 // VIEWS - SWIG
-server.register(require('vision'), (err) => {
+server.register(require('vision'), function (err) {
 
     if (err) {
         throw err;
     }
-
-    server.views({
-        engines: {
-            html: require('swig')
-        },
-        path: process.myEnv.views_folder
-    });
+    
+    var engine= {};
+    engine.engines = {};
+    engine.engines[templateEngine[templateEngine.main].fileType] = templateEngine[templateEngine.main].extending || templateEngine[templateEngine.main].instance;
+    engine.path = templateEngine[templateEngine.main].path || process.myEnv.views_folder
+    
+    server.views(engine);
 });
 
 // ROUTE
