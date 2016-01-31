47559c51543fbaaafcb95c20e2ae73bf886b2a1e
lesspion <coquelet.c@gmail.com>
2016-01-31 22:21:45 +0100

    added load file to autoload route

---
+++ tools/loader.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+
+var load = {};
+load.route = function (server) {
+    var routes = require(process.myEnv.config_folder + '/routes');
+    for (var i in routes) {
+        var config = routes[i].config || {};
+        var handler;
+        if (typeof routes[i].handler !== "function") {
+            var tempHandler = routes[i].handler.split('.');
+            var namespace = tempHandler[0];
+            var action = tempHandler[1];
+            handler = this.routeAction()[namespace][action];
+        } else {
+            handler = routes[i].handler;
+        }
+        server.route({
+            method: routes[i].method,
+            path: i,
+            handler: handler,
+            config: config
+        });
+    }
+};
+
+load.routeAction = function () {
+    var allAction = fs.readdirSync(process.myEnv.action_folder);
+    var routesAction = {};
+    for (var i = 0; i < allAction.length; i++) {
+        var tempObjectAction = require(process.myEnv.action_folder + '/' + allAction[i]);
+        routesAction[allAction[i].substr(0, allAction[i].length - 3)] = tempObjectAction;
+    }
+    return routesAction;
+};
+
+module.exports = load;
