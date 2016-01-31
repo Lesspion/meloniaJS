95f9c45392c7c9211701855425f5c5c41ee24cd7
lesspion <coquelet.c@gmail.com>
2016-01-31 22:22:33 +0100

    add add my env file to load all main config

---
+++ MyEnv.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "port": 8080,
+    "host": "0.0.0.0",
+    "domain_name": 'lesspion.fr',
+    "base_folder": __dirname,
+    "views_folder": __dirname + '/views',
+    "assets_folder": __dirname + "/assets",
+    "config_folder": __dirname + '/config',
+    "tools_folder": __dirname + '/tools',
+    'action_folder': __dirname + '/routesAction',
+    
+};
