ff17108bfb8ea6f39b2345f59199a9c5afebf000
lesspion <coquelet.c@gmail.com>
2016-02-01 01:02:00 +0100

    remove useless parameter

--- config/templateEngine.js
+++ config/templateEngine.js
@@ -4,6 +4,6 @@ module.exports = {
         'fileType': 'html',
         'path': process.myEnv.views_folder,
         "instance": require('swig'),
-        "extending": require(process.myEnv.tools_folder + '/loader').loadSwigExtension(this.instance)
+        "extending": require(process.myEnv.tools_folder + '/loader').loadSwigExtension()
     }
 }
