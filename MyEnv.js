58720205204d0f304cfe6ce132000651d6887373
= <=>
2016-04-04 23:43:25 +0200

    add db -schema-validation && helper folder

--- MyEnv.js
+++ MyEnv.js
@@ -3,6 +3,7 @@ module.exports = {
     "host": process.env.IP || "0.0.0.0",
     "domain_name": 'localhost',
     "template-engine": "swig",
+    "db-engine": "mongodb",
     "base_folder": __dirname,
     "views_folder": __dirname + '/views',
     "assets_folder": __dirname + "/assets",
@@ -10,4 +11,7 @@ module.exports = {
     "tools_folder": __dirname + '/tools',
     'action_folder': __dirname + '/routesAction',
     'bower_folder': __dirname + '/bower_components',
+    'dbSchema': __dirname + '/database/schema',
+    'dbValidation': __dirname + '/database/validation',
+    'helper': __dirname + '/helper'
 };
