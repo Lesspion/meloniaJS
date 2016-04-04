a2f0d16042f780fdf254e440eaecde5dba9261de
= <=>
2016-04-04 23:46:30 +0200

    add test route to create user

--- routesAction/user.js
+++ routesAction/user.js
@@ -1,5 +1,23 @@
 module.exports = {
     'index': function (request, reply) {
-        reply('hello : ' + encodeURIComponent(request.params.username));
+        var User = process.schema.models.User;
+        User.find(function (err, users) {
+            reply('hello : ' + encodeURIComponent(request.params.username) + '<br>Users: ' + JSON.stringify(users));
+        });
+    },
+    'add': function (request, reply) {
+        var User = process.schema.models.User;
+        var user = new User();
+        user.name = request.params.name;
+        console.log(user.name);
+        console.log(user.isValid());
+        if (!user.isValid())
+            reply('an error occcured');
+                
+        user.save(function (err) {
+            if (err)
+                console.log(err);
+            reply('user saved');
+        });
     }
 };
